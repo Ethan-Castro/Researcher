a3a077f73a51100beeb016882cbed0e81004afc4
Ethan <theethancastro@gmail.com>
2024-05-27 13:27:39 -0400

    Update researcher.py

--- researcher.py
+++ researcher.py
@@ -1,22 +1,23 @@
+# Import the required libraries
 import streamlit as st
-import phidata as phi
-import openai as openai
-import arxiv as arxiv
 from phi.assistant import Assistant
 from phi.llm.openai import OpenAIChat
 from phi.tools.arxiv_toolkit import ArxivToolkit
 
+# Set up the Streamlit app
 st.title("Chat with Arxiv 🔎🤖")
 st.caption("This app allows you to chat with arXiv using OpenAI GPT-4o model.")
-openai_access_token = st.text_input("OpenAI_API_Key", type="password")
 
-#If OpenAI API key is provided, create an instance of Assistant
+# Get OpenAI API key from user
+openai_access_token = st.text_input("OpenAI API Key", type="password")
+
+# If OpenAI API key is provided, create an instance of Assistant
 if openai_access_token:
     # Create an instance of the Assistant
     assistant = Assistant(
     llm=OpenAIChat(
         model="gpt-4o",
-        max_tokens=500,
+        max_tokens=1024,
         temperature=0.9,
         api_key=openai_access_token) , tools=[ArxivToolkit()], show_tool_calls=True
     )
